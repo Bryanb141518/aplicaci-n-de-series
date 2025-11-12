dffa8081775344da357a135425fafdc76cedc558
brayan benitez <bryan141518@gmail.com>
2025-11-12 10:41:06 -0500

    creacion de clase usaurios integrando raise y property

--- proyectoseriespoo.py
+++ proyectoseriespoo.py
@@ -1,3 +1,23 @@
+class Usuario:
+    def __init__(self, nombre: str, apellido: str, edad: int, correo:str, contrasena:str):
+        self._nombre = nombre
+        self._apellido = apellido
+        self._edad = edad
+        self.__correo = correo
+        self.__contrasena = contrasena
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nuevo_nombre):
+        if not isinstance(nuevo_nombre, str):
+            raise TypeError("El nombre debe ser texto.")
+        if not nuevo_nombre.strip():
+            raise ValueError("El nombre no puede estar vacío.")
+        self._nombre = nuevo_nombre
+
 class Series:
     def __init__(self, nombre, genero: str,calificacion:float = None,estado:str ="pendiente",
                  plataforma:str = None,año:int = None,descripcion:str = None):
