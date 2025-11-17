67538d00eba0de21dbef77c5b1d425e2777d5ff6
brayan benitez <bryan141518@gmail.com>
2025-11-17 11:57:40 -0500

    organizacionde carpetas e instalacion de api

---
+++ main.py
@@ -0,0 +1,7 @@
+# importar clases de lógica
+from logic.usuario import Usuario
+from logic.serie import Series
+
+# importar clases de Pydantic (validación)
+from models.usuario_model import UsuarioModel
+from models.serie_model import SerieModel
