82dc5753ee7976da9509b1c251688c2e4817d5b5
brayan benitez <bryan141518@gmail.com>
2025-11-20 10:53:34 -0500

    se completo la validacion de los datos de la clase series y se creo el post para guardar la instancia de la clase

--- models/series.py
+++ models/series.py
@@ -1,10 +1,10 @@
+
 from datetime import datetime
-from http.client import HTTPException
-from wsgiref.validate import validator
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field, field_validator
 
-from fastapi import FastAPI
-from pydantic import BaseModel, Field, constr, conint
-
+# Inicializar la app de FastAPI
+app = FastAPI()
 lista_series = []
 
 genero_validos = ["terror","drama"]
@@ -25,23 +25,49 @@ class SeriesModel(BaseModel):
 
 # validacion de que el dato sea de tipo str que contenga almenos un caracter y maximo 100
 
-    nombre : constr(min_length=1, max_length=100)
+    nombre : str = Field(min_length=1, max_length=100)
 
-    genero : constr(min_length=1, max_length=100)
+    genero : str = Field(min_length=1, max_length=100)
 
-    plataforma : constr(min_length=1, max_length=100) = "Desconocida"
+    plataforma: str = Field(default="Desconocida", min_length=1, max_length=100)
+
+    año : int = Field(..., ge=1920, le=2028)
+
+    descripcion : str = Field(min_length=100, max_length=1000)
 
 
 
-    @validator('nombre')
+    @field_validator('nombre', mode= "before")
     def validate_nombre(cls, v):
-        return v.strip().title()
+        cleaned = v.strip()
+        if not cleaned:
+            raise ValueError("este campo no puede ir vacio")
+        return cleaned.title()
 
-    @validator('genero')
+    @field_validator('genero',mode= "before")
     def validate_genero(cls, v):
-        if v.lower() not in genero_validos:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Género '{v}' no permitido. Géneros válidos: {genero_validos}"
+        cleaned = v.strip().lower()
+
+        if cleaned not in genero_validos:
+            raise ValueError(
+                f"genero {v} no permitido generos invalidos {' '.join(genero_validos)}"
             )
-        return v.strip().lower()
+        return cleaned
+
+    @field_validator('descripcion', mode= "before")
+    def validate_descripcion(cls, v):
+        cleaned = v.strip().title()
+        if not cleaned:
+            raise ValueError("no puede conteenr solo espacios vacios")
+        return cleaned
+
+@app.post("/series",status_code=201)
+
+def create_series(series: SeriesModel):
+    nueva_serie = series.model_dump()
+
+    lista_series.append(nueva_serie)
+
+    return {"mensaje": "Serie creada correctamente", "serie": nueva_serie}
+
+
