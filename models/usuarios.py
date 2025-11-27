aca93066e4438713388c05d05d63de48d01ab9ca
brayan benitez <bryan141518@gmail.com>
2025-11-27 10:04:58 -0500

    creacion del validator para validar el nombre del usuario

--- models/usuarios.py
+++ models/usuarios.py
@@ -28,7 +28,31 @@ class Usuariomodel(BaseModel):
     contrasena : str = Field(min_length=1, max_length=100)
 
 
-   
+    @field_validator("nombre", mode="before")
+    def validate_nombre(cls, v):
+        limpiar = v.strip()
+        if not limpiar:
+            raise ValueError("este campo no puede ir vacio")
+        # recorremos el el texto con un for validando carater por carater para que no tenga numero
+        #con any lo que hace es validar si almenos uno es True
+
+        if any(char.isdigit() for char in limpiar):
+            raise ValueError("el nombre no puede contener numeros")
+
+        #no se permite simbolos ni carateres raros
+        if not limpiar.replace(" ", "").isalpha():
+            raise ValueError("el nombre solo puede contener letras y espacios")
+
+        #el  nombre no puede contener menos de dops caracteres ni mas de 50
+        if not (2 <= len(limpiar) <= 50):
+            raise ValueError("el nombre debe tener entre 2 y 50 caracteres")
+
+         # No permitir espacios internos por que solo es una palabra
+        if " " in limpiar:
+            raise ValueError("el nombre debe ser una sola palabra (sin espacios)")
+
+        return limpiar.title()
+
 
 
 
