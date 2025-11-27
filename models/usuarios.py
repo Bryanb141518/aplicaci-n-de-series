cf23fa4bc6bd6fbec0306d4221a3fc94fd7ff25d
brayan benitez <bryan141518@gmail.com>
2025-11-27 10:27:36 -0500

    creacion de la funcion para la validacion de los datos nombre y apellido

--- models/usuarios.py
+++ models/usuarios.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class Usuario:
-    def __init__(self, nombre: str, apellido: str, edad: int, correo:str, contrasena:str):
+    def __init__(self, nombre: str, apellido: str,  edad: int, correo:str, contrasena:str):
 
         self._nombre = nombre
         self._apellido = apellido
@@ -13,13 +13,37 @@ class Usuario:
         self.__correo = correo
         self.__contrasena = contrasena
 
+#funcion para validar de una vez nombre y apellido
+def validar_nombre_apellido(valor: str,campo: str) -> str:
+    limpiar = valor.strip()
+
+    if not limpiar:
+        raise ValueError(f"este {campo} no puede ir vacio")
+
+    if any(char.isdigit() for char in limpiar):
+        raise ValueError(f"el {campo} no puede contener numeros")
+
+    if not limpiar.replace(" ", "").isalpha():
+        raise ValueError(f"el {campo} solo puede contener letras y espacios")
+
+    if not (2 <= len(limpiar) <= 50):
+        raise ValueError(f"el {campo} debe tener entre 2 y 50 caracteres")
+
+    if " " in limpiar:
+        raise ValueError(f"el {campo} debe ser una sola palabra sin espacios")
+
+    return limpiar.title()
+
 #clase model es la que tomara como referencia flastapi
+
 
 class Usuariomodel(BaseModel):
 
     nombre : str = Field(min_length=1, max_length=100)
 
     apellido : str = Field(min_length=1, max_length=100)
+
+    ciudad : str = Field(min_length=1, max_length=100)
 
     edad : int = Field(..., ge=5, le=120)
 
@@ -28,30 +52,14 @@ class Usuariomodel(BaseModel):
     contrasena : str = Field(min_length=1, max_length=100)
 
 
-    @field_validator("nombre", mode="before")
-    def validate_nombre(cls, v):
-        limpiar = v.strip()
-        if not limpiar:
-            raise ValueError("este campo no puede ir vacio")
-        # recorremos el el texto con un for validando carater por carater para que no tenga numero
-        #con any lo que hace es validar si almenos uno es True
+    @field_validator("nombre",mode="before")
+    def validar_nombre(cls, v):
+        return validar_nombre_apellido(v,"nombre")
 
-        if any(char.isdigit() for char in limpiar):
-            raise ValueError("el nombre no puede contener numeros")
+    @field_validator("apellido",mode="before")
+    def validar_apellido(cls, v):
+        return validar_nombre_apellido(v,"apellido")
 
-        #no se permite simbolos ni carateres raros
-        if not limpiar.replace(" ", "").isalpha():
-            raise ValueError("el nombre solo puede contener letras y espacios")
-
-        #el  nombre no puede contener menos de dops caracteres ni mas de 50
-        if not (2 <= len(limpiar) <= 50):
-            raise ValueError("el nombre debe tener entre 2 y 50 caracteres")
-
-         # No permitir espacios internos por que solo es una palabra
-        if " " in limpiar:
-            raise ValueError("el nombre debe ser una sola palabra (sin espacios)")
-
-        return limpiar.title()
 
 
 
